719d4e8b2dc4dae516198c045ce3d37400f75da1
Helder Pereira <hpereira@uolinc.com>
2022-01-06 09:39:51 -0300

    Adicionar base sobre ansible, update p/ c9 script

--- cloud9/scripts/config.sh
+++ cloud9/scripts/config.sh
@@ -1,14 +1,18 @@
 
 #!/bin/bash
-test -d $HOME/environment/info || install -d -m 0700 -o ubuntu -g ubuntu $HOME/environment/info
+test -d $HOME/environment/info || install -d -m 0700 -o ec2-user -g ec2-user $HOME/environment/info
 
 # Identificando o endereco pub da instancia:
-curl -s http://169.254.169.254/latest/meta-data/public-ipv4 -o $HOME/environment/info/PUBLIC_IP.txt && chown ubuntu: $HOME/environment/info/PUBLIC_IP.txt
-curl -s http://169.254.169.254/latest/meta-data/public-hostname -o $HOME/environment/info/PUBLIC_DNS.txt && chown ubuntu: $HOME/environment/info/PUBLIC_DNS.txt
+curl -s http://169.254.169.254/latest/meta-data/public-ipv4 -o $HOME/environment/info/PUBLIC_IP.txt && chown ec2-user: $HOME/environment/info/PUBLIC_IP.txt
+curl -s http://169.254.169.254/latest/meta-data/public-hostname -o $HOME/environment/info/PUBLIC_DNS.txt && chown ec2-user: $HOME/environment/info/PUBLIC_DNS.txt
 
 
 # Resizing para o disco local do ambiente:
-sh $HOME/environment/mba_devsecops/cloud9/scripts/resize.sh 20 > /dev/null
+sh $HOME/environment/codecommit/scripting/resize.sh 20 > /dev/null
+
+# Instalação de componentes:
+printf "\n Configurando Dependencias \n"
+sudo yum install -y tmux jq
 
 # Instalando o docker + docker-compose
 printf "\n Instalando o docker-compose \n"
@@ -19,3 +23,12 @@ sudo chmod +x /usr/local/bin/docker-compose
 printf "\n Instalando o cliente do Kubernetes \n"
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+printf "\n Instalando o ansible via pip \n"
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+sudo python -m pip install --upgrade pip
+pip install ansible boto3 boto
+
+printf "\n Configurando Enviroment Env \n"
+echo "AWS_REGION=us-east-2" >> $HOME/.bashrc
+source $HOME/.bashrc
--- cloud9/scripts/remote.sh
+++ cloud9/scripts/remote.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+# Referencia:
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/authorize-security-group-ingress.html
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#retrieving-the-public-key
+
 printf "Criando chave para o acesso remoto \n"
 
-test -f $HOME/environment/info/id_seglab.pem || cat /dev/zero | ssh-keygen  -C "ubuntu@remote" -q -N "" -b 4096 -f $HOME/environment/info/id_seglab.pem && chown ubuntu: $HOME/environment/info/id*
+test -f $HOME/environment/info/id_lab.pem || aws ec2 create-key-pair --key-name "id_lab" | jq -r ".KeyMaterial" > $HOME/environment/info/id_lab.pem && chmod 600 $HOME/environment/info/id*
+test -f $HOME/environment/info/id_lab.pub || ssh-keygen -y -f $HOME/environment/info/id_lab.pem > $HOME/environment/info/id_lab.pub
+
 export PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 export PUBLIC_DNS=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 
 printf "Configurando a chave publica"
-file $HOME/environment/info/id_seglab.pem.pub && cat $HOME/environment/info/id_seglab.pem.pub >> $HOME/.ssh/authorized_keys
+file $HOME/environment/info/id_lab.pem.pub && cat $HOME/environment/info/id_lab.pem.pub >> $HOME/.ssh/authorized_keys
 
 
 printf "Identificando o SecurityGroup do projeto"
 aws ec2 describe-security-groups --filters Name=group-name,Values=*aws-cloud9* --query "SecurityGroups[*].[GroupName]" --output table
 
-
-# Recording Security Group as Variable
+# Definindo os SGs
 SG_NAME=$(aws ec2 describe-security-groups --filters Name=group-name,Values=*aws-cloud9* --query "SecurityGroups[*].[GroupName]" --output text)
+DEFAULT_SG=$(aws ec2 describe-security-groups --filters Name=group-name,Values=default --query "SecurityGroups[*].[GroupName]" --output text)
 
-printf "Qual o end. de rede publico de sua oriem para acesso remoto? Coloque apenas o endereço sem a mascara de rede \n"
+
+
+printf "Qual o end. de rede publico de sua origem para acesso remoto? Coloque apenas o endereço sem a mascara de rede \n"
 read REMOTE_PUBLIC_IP
 
+# Regras para SSH
 aws ec2 authorize-security-group-ingress --group-name $SG_NAME --protocol tcp --port 0-65535 --cidr $REMOTE_PUBLIC_IP/32
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol tcp --port 0-65535 --cidr $REMOTE_PUBLIC_IP/32
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol tcp --port 0-65535 --source-group $SG_NAME
 
-cat $HOME/environment/info/id_seglab.pem
+# Regras para ICMP
+aws ec2 authorize-security-group-ingress --group-name $SG_NAME --protocol icmp --port -1 $REMOTE_PUBLIC_IP/32
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol icmp --port -1 $REMOTE_PUBLIC_IP/32
+aws ec2 authorize-security-group-ingress --group-name $DEFAULT_SG --protocol icmp --port -1 --source-group $SG_NAME
+
+
+cat $HOME/environment/info/id_lab.pem
 echo ""
 printf "Copie a chave privada para sua maquina local e utilize no acesso remoto ao ip $PUBLIC_IP ou $PUBLIC_DNS \n"
